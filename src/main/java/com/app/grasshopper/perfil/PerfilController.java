f8144acfc8f3245c1ed3025f53a6441e0bf93c38
Brandon Caranqui <brandito144@gmail.com>
2023-11-11 18:10:30 -0500

    TODO

--- src/main/java/com/app/grasshopper/perfil/PerfilController.java
+++ src/main/java/com/app/grasshopper/perfil/PerfilController.java
@@ -22,25 +22,36 @@ public class PerfilController {
     @Autowired
     private PerfilService perfilService;
 
+    //CREAR
+
     @PostMapping("/")
     public Perfil save(@RequestBody Perfil entity) {
         return perfilService.save(entity);
     }
+
+
+    //BUSCAR POR ID
 
     @GetMapping("/{id}/")
     public Perfil findById(@PathVariable long id){
         return perfilService.findById(id);
     }
 
+    //ACTUALIZAR
+
     @PutMapping("/")
     public Perfil update(@RequestBody Perfil entity){
         return perfilService.save(entity);
     }
 
+    //ELIMINAR POR ID
+
     @DeleteMapping("/{id}/")
     public void deleteById(@PathVariable long id){
         perfilService.deleteById(id);
     }
+
+    //BUSCAR TODO
 
     @GetMapping("/")
     public List<Perfil> findAll() {
